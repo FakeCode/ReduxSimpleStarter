2076f3e56fa0eceae1eeb829d94e76f2b73ab3f7
Umesh <umesh.services@gmail.com>
2016-10-20 23:11:02 -0500

    api added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import SearchBar from './components/search_bar';
+import Youtube from 'youtube-api-search';
+import VideoList from './components/video_list'
 
-const App = () => {
-    return <div> Hi umesh </div>;
+const ApiKey = ''; //Add youtube api key
+
+
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = { videos: [] };
+        Youtube({key: ApiKey, term: 'umesh maharjan'}, (videos) =>
+        {
+            this.setState({ videos });
+        });
+    }
+render() {
+    return(
+        <div>
+        <SearchBar />
+        <VideoList videos = { this.state.videos } />
+        </div>
+    );
+}
 }
 
 ReactDom.render(<App />, document.querySelector('.container'));
